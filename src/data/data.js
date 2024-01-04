ac612647f5dc95a8f44d5a4829cb37d3d053862e
Jenil Patel <94772560+pateljenil139@users.noreply.github.com>
2024-01-04 12:41:55 +0530

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+
+
+export const data = [
+  
+  
+  {
+    id: 3,
+    img: 'https://www.seekpng.com/png/full/7-79244_coming-soon-png-illustration.png',
+    title: "Coming  Soon...",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: 'https://www.pngall.com/wp-content/uploads/2016/04/WhatsApp-PNG-Picture.png',
+    title: "Whatsapp",
+    desc:"This Is A Real Time Chat App Like a Whatsapp",
+    link: "",
+    git: "https://github.com/pateljenil139/wpbyjenilpatel",
+  },
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/t4JhVGR1/Screenshot-2023-08-31-154956.png',
+    title: "Amazon Clone",
+    desc: "This Is Only Desktop Freindly Website ",
+    link: "https://app-f222a.web.app/",
+    git: "https://github.com/pateljenil139/amazon-clone.git",
+  },
+  
+];
