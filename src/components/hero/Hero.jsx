d43e430b04385a4c855e69fff0f3e1060eb72044
Jenil Patel <94772560+pateljenil139@users.noreply.github.com>
2024-10-17 23:06:15 +0530

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -106,7 +106,6 @@ const Hero = () => {
     marginTop: "100px",
     width: "400px",
     height: "400px",
-    margin: "0 auto",
     borderRadius: "25px", // Add rounded corners with desired radius
     boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.25)", // Add subtle fading shade
   }}
